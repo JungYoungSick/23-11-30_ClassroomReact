f58d2cac66b6ef9a793526d295186ecefcfe6be8
JungYoungSick <junfas1216@gmail.com>
2023-11-12 22:33:34 +0900

    React_1 작성 완료

---
+++ React_1/app.js
@@ -0,0 +1,22 @@
+function conponent(elementNode, attributes, children) {
+  let elementStr = `<${elementNode}`;
+  for (let key in attributes) {
+    elementStr += `${key}="${attributes[key]}"`;
+  }
+  elementStr += '>';
+  if (children) {
+    children.forEach((child) => {
+      if (typeof child === 'string') {
+        elementStr += child;
+      } else {
+        elementStr += component(child.elementNode, child.attributes, child.children);
+      }
+    })
+  }
+  elementStr += `</${elementNode}>`;
+  return elementStr;
+}
+let test = component('div', { style: 'color:blue;' }, [
+  component('h1', {}, ['This is page 1'])
+])
+console.log(test)
